ad44e796bce47cb8968c0b9eeb2a5b2c24e46c0c
Ravi Mohan <ravi.mohan@roonga.com.au>
2017-10-29 06:25:10 +1100

    add code

---
+++ BackendApi/Controllers/ValuesController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using BackendApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendApi.Controllers
+{
+    [Route("api/admin/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Back", "End" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return (id * id).ToString();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public Student Post([FromBody]Student student)
+        {
+	        student.Name = "Thank you";
+
+			return student;
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ FrontEndApi/Startup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FrontEndApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+	    private static bool IsAdminPath(HttpContext httpContext)
+	    {
+		    return httpContext.Request.Path.Value.StartsWith(@"/api/admin/", StringComparison.OrdinalIgnoreCase);
+	    }
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+			// Install-Package Microsoft.AspNetCore.Proxy
+			app.MapWhen(IsAdminPath, builder => builder.RunProxy(new ProxyOptions
+	        {
+		        Scheme = "http",
+		        Host = "localhost",
+		        Port = "55329"
+			}));
+
+	        app.Use(async (context, next) =>
+	        {
+		        await context.Response.WriteAsync("Hello from Frond End");
+		        return;
+	        });
+
+			app.UseMvc();
+        }
+    }
+}
